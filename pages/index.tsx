2655dfb23c34526678fa2842eb462c3f7e5c38b6
amanda <amandareisdw@gmail.com>
2023-02-23 00:46:10 -0300

    Created image to test in production

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { ReactElement } from 'react'
+import Oscar from '../components/Oscar'
 
 export default function Home (): ReactElement {
   return (
         <div>
+            <Oscar />
             <h1>Testando...</h1>
         </div>
   )
