c950ce528226c6e96a26084c44be0f160dc0abf6
amanda <amandareisdw@gmail.com>
2023-02-23 18:14:24 -0300

    Created TopMenu component

---
+++ components/Home/TopMenu/ShadowGradient.tsx
@@ -0,0 +1,8 @@
+import type { ReactElement } from 'react'
+import style from '../../../styles/home/top-menu/ShadowGradient.module.scss'
+
+export default function ShadowGradient (): ReactElement {
+  return (
+    <div className={style.shadow}></div>
+  )
+}
