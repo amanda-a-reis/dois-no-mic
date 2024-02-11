0cdb69a236cd02ad948431a02621e983495a94f1
Amanda Reis <amandareisdw@gmail.com>
2024-02-11 11:35:11 -0300

    feat: add styled-components theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
-import '../styles/global.css'
+import "../styles/global.css"
+import { ThemeProvider } from "styled-components"
+import theme from "../styles/theme/theme"
 
-export default function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
---
+++ styles/theme/theme.ts
@@ -0,0 +1,11 @@
+const color = {
+  yellow: "#EDCE74",
+  gray_primary: "#3F3F3F",
+  gray_secondary: "#2F2F2F",
+  black: "#000000",
+  white: "#FFFFFF"
+}
+
+const theme = { color }
+
+export default theme
