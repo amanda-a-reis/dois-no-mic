ceeb9c34d23b22442208ef3cba0b0339e1854a7d
Amanda Reis <amandareisdw@gmail.com>
2024-02-11 18:34:51 -0300

    fix: global size

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Img = styled(Image)`
 
 const Container = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
