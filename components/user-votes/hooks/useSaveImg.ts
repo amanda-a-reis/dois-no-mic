e12102b12bf92374a1fc8bb8bcbbc7ad0b4a79ca
Amanda Reis <amandareisdw@gmail.com>
2024-02-25 15:51:36 -0300

    change lib to generate png image

--- components/user-votes/hooks/useSaveImg.ts
+++ components/user-votes/hooks/useSaveImg.ts
@@ -1,34 +1,42 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
-import { toPng } from "html-to-image"
+import html2canvas from "html2canvas"
 import { useCallback, useRef } from "react"
 
 const useSaveImg = () => {
   const ref = useRef<HTMLDivElement>(null)
 
-  const exportAsImage = useCallback(() => {
-    if (ref.current === null) {
-      return
-    }
-
+  const downloadImg = useCallback((blob) => {
     const timestamp = Date.now().toString()
 
     const uniqueNumber = timestamp.slice(-6)
 
     const fileName = `meus-votos-oscar-2024-${uniqueNumber}.png`
 
-    toPng(ref.current, {
-      cacheBust: true
-    })
-      .then((dataUrl) => {
-        const link = document.createElement("a")
-        link.download = fileName
-        link.href = dataUrl
-        link.click()
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [ref])
+    const fakeLink = window.document.createElement("a")
+    const fakeLinkStyle = fakeLink.style as CSSStyleDeclaration
+    fakeLinkStyle.display = "none"
+    fakeLink.download = fileName
+
+    fakeLink.href = blob
+
+    document.body.appendChild(fakeLink)
+    fakeLink.click()
+    document.body.removeChild(fakeLink)
+
+    fakeLink.remove()
+  }, [])
+
+  const exportAsImage = useCallback(async () => {
+    if (ref.current === null) {
+      return
+    }
+
+    const canvas = await html2canvas(ref.current)
+
+    const image = canvas.toDataURL("image/png", 1.0)
+
+    downloadImg(image)
+  }, [ref, downloadImg])
 
   return {
     exportAsImage,
